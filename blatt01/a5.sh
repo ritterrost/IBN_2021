33ab5c382a22c61cac9b0c0fcb9fd0cd943c6a51
Till Rostalski <mr453@uni-heidelberg.de>
2021-05-03 18:52:17 +0200

    fixed some bugs

--- blatt01/a5.sh
+++ blatt01/a5.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+
+if [ -f ./a5 ]
+then 
+   continue
+else 
+    echo "please first compile the file: \"a5.cc\"!"
+    exit 1 
+fi
+
+
 VAR1=0
 VAR2=0
 for i in {1..100}
 do
     echo "Execution number: $i"
-    ./b1
+    ./a5
     if [ $? = 0 ]
     then VAR1=$((VAR1+1)) 
     else VAR2=$((VAR2+1)) 
