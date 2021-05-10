b518450500e97af1ddbf695ac95a49b5f38b992e
Luis Schulte <schulte4321l@outlook.de>
2021-05-10 22:42:33 +0200

    🎉 init

---
+++ ueb02/aufgabe5.c
@@ -0,0 +1,24 @@
+/**
+ * @file aufgabe5.c
+ * @author Gregor N. (@), Till R. (@ritterrost), Luis S. (@lsg551)
+ * @brief Aufgabenbearbeitung zu Aufgabe 5 des Übungsblattes 2 vom 04.05.2021
+ * @version 0.1
+ * @date 10.05.2021
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+/**
+ * @brief Entry point
+ * 
+ * Aufgabe :
+ * 
+ * Parent -> SM(pid1) -> Child
+ * 
+ */
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
