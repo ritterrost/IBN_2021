fd9778058a7c45873a952aa39e2dc827dd46038f
Luis Schulte <schulte4321l@outlook.de>
2021-05-11 10:13:53 +0200

    🚧

--- ueb02/aufgabe5.c
+++ ueb02/aufgabe5.c
@@ -9,16 +9,74 @@
  * 
  */
 
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <string.h>
+
 /**
  * @brief Entry point
  * 
  * Aufgabe :
- * 
- * Parent -> SM(pid1) -> Child
+ * Parent erzeugt Child und teilt mit diesem via SM die pid des Kindes.
+ * Child gleicht diese mit seiner ab, gibt Antwort an Parent.
  * 
  */
 int main(int argc, char *argv[])
 {
+    int sharedMemoryId = shmget(IPC_PRIVATE, 300, S_IRUSR | S_IWUSR);
+    int forkId = fork();
+
+    if (forkId <= -1)
+    { // error
+        printf("Creating a child process was unsuccessful");
+    }
+    else if (forkId == 0)
+    { // child process
+        char *sharedMemory = (char *)shmat(sharedMemoryId, NULL, 0);
+        // read pid sent from parent
+        char ownPidFromSharedMemory = *sharedMemory;
+        // get own pid
+        int ownPid = getpid();
+        // evaluate
+        if ((int)ownPidFromSharedMemory == (int)ownPid)
+        {
+            sprintf(sharedMemory, "OK");
+        }
+        else
+        {
+            sprintf(sharedMemory, "FALSE");
+        }
+        // detach
+        shmdt(sharedMemory);
+    }
+    else
+    { // parent process
+        char *sharedMemory = (char *)shmat(sharedMemoryId, NULL, 0);
+        // get pid of child
+        int childPid = getpid();
+        // send child pid to sm
+        sprintf(sharedMemory, (char *)childPid);
+        // read answer
+        char answer = *sharedMemory;
+        // process answer
+        if (strcmp(answer, "OK") == 0)
+        {
+            printf("Evaluation succeeded: pid are equal");
+        }
+        else if (strcmp(answer, "FALSE") == 0)
+        {
+            printf("Evaluation succeeded: pid are not equal");
+        }
+        else
+        { // error
+            printf("Unable to evaluate");
+        }
+        // detach
+        shmdt(sharedMemory);
+    }
 
     return 0;
 }
